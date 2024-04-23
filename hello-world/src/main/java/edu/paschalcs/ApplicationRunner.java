8de9d878e8ea177ad89aa168cb3755add2ffc25c
GabrielT07 <165056396+GabrielT07@users.noreply.github.com>
2024-04-23 16:56:38 +0000

    ApplicationRunner class creation

---
+++ hello-world/src/main/java/edu/paschalcs/ApplicationRunner.java
@@ -0,0 +1,15 @@
+package edu.paschalcs;
+
+import com.google.common.math.IntMath;
+
+/**
+ * Example main src.
+ *
+ * @author 
+ */
+class ApplicationRunner {
+
+    public static void main(String[] args){
+        
+    }
+}
